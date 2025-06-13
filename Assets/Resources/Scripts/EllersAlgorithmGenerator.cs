a281decf0e4e90a220ca35b2cd198df037ae554d
BNOK <nabli.hamza112@gmail.com>
2025-06-13 03:21:08 +0000

    Update EllersAlgorithmGenerator.cs

--- Assets/Resources/Scripts/EllersAlgorithmGenerator.cs
+++ Assets/Resources/Scripts/EllersAlgorithmGenerator.cs
@@ -2,6 +2,11 @@ using NUnit.Framework;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+/* Spawn cells on X axis then change rows by changing Z 
+ * DO NOT CREATE THE MAZE ON THE Z BY DEFAULT TO NOT BE CONFUSED ON THE VISUALS
+ */
+
 public class EllersAlgorithmGenerator : MonoBehaviour
 {
     [SerializeField]
@@ -18,25 +23,56 @@ public class EllersAlgorithmGenerator : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        InitMaze(_mazeWidth, _mazeHeight);
+        
     }
 
-    private void InitMaze(int width, int height)
+    private MazeCell[] CreateRow(int rowindex)
     {
-        MazeCell[] firstrow = new MazeCell[width];
+        MazeCell[] result = new MazeCell[_mazeWidth];
 
-        for(int i = 0; i < width; i++)
+        for(int ID = 0; ID < _mazeWidth; ID++)
         {
-            firstrow[i] = Instantiate(_cellPrefab, new Vector3(i, 0, height), Quaternion.identity, transform);
-            firstrow[i].cellID = i + height;
-            firstrow[i].cellIndex = new int[] {i, height};
+            MazeCell cell = new MazeCell();
+            cell.cellID = ID;
+            cell.cellIndex = new int[] {ID, rowindex};
+            result[ID] = cell;
+        }
 
 
+        return result;
+    }
+
+    private void MergeRow( ref MazeCell[] currentrow)
+    {
+        System.Random random = new System.Random();
+        for(int index = 0; index < currentrow.Length -1; index++)
+        {
+            if (random.Next(0,2) == 1 && (currentrow[index].cellID != currentrow[index + 1].cellID))
+            {
+                if(random.Next(0,2) == 1)
+                {
+                    currentrow[index + 1].cellID = currentrow[index].cellID;
+                }
+                else
+                {
+                    currentrow[index].cellID = currentrow[index + 1].cellID;
+                }
+            }
         }
     }
 
-    private void CreateRow(MazeCell[] previousrow, MazeCell[] currentrow)
+    private void ClearVerticalWalls(MazeCell[] row)
     {
-        
+        MazeCell currentcell = row[0];
+        for(int index = 1;index < row.Length;index++)
+        {
+            if (row[index].cellID == currentcell.cellID)
+            {
+                row[index].ClearLeftWall();
+                currentcell.ClearRightWall();
+            }
+
+            currentcell = row[index];
+        }
     }
 }
